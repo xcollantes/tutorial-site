3ac190f199c06d114b2554f9926b3bfaa7eb66cf
xcollantes <xcollantes@zagmail.gonzaga.edu>
2019-01-26 06:08:47 +0000

    Create separate repo

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<!-- 
+	Xavier Collantes,
+	01/11/2019,
+	tutor.php
+-->
+
+<html lang='en'>
+<head>
+	<title>Python Tutoring</title>
+	<meta name="keywords" content="python tutorial beginner business information management school gonzaga computer science">
+	<meta name="description" content="Xavier's Python Tutorial">
+	
+	<meta name="author" content="Xavier Collantes">
+	
+	<!--============ IE FIX==============-->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!--==============FIRST MOBILE=============-->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Include Bootstrap And Font Awesome -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">        
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	
+	
+	<!-- Include Simple Light Box -->
+	<link rel="stylesheet" href="assets/css/simplelightbox/simplelightbox.css">
+	
+	<!-- Include Favicon -->
+	<link rel="shortcut icon" href="assets/image/favicon.jpg">
+	
+	<!-- Include Google Font -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,600,700,800,900">
+
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Passion+One">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:700">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trocchi">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu:700">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Libre+Franklin:300">
+
+	<!-- Include Google Material Icons -->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+	<link rel="stylesheet" href="tutor.css">
+	<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+	
+</head>
+
+<body data-spy="scroll">
+
+	<!-- Sidenav -->
+	<div class="sidenav">
+		<a href="#">Home</a>
+		<a href="#">About</a>
+		<a href="#">Contact</a>
+		<a href="#">Lessons</a>
+	</div>
+	<!-- /Sidenav -->
+	
+	<section id="header">
+		<video autoplay muted loop>
+			<source src="assets-tutor/meet.mp4" type="video/mp4">
+		</video>
+		<h1 class="big-letter theX"><span class="inner-letter">X</span></h1>
+		<h1 class="big-letter theV"><span class="inner-letter">V</span></h1>
+		<!--h1 class="big-letter theR">R</h1-->
+		<div class="container" id="header-container">
+			<h1 class="header-item header-title"><span>Xavier Collantes</span></h1>
+			<h1 class="header-item">Description</h1>
+			
+		</div>
+		
+		
+		
+		<div class="container" id="panel-container">
+			<div class="panel-item">
+				<h2>thing 1</h2>
+			</div>
+			
+			<div class="panel-item">
+				<h2>thing 2</h2>
+			</div>
+			
+			<div class="panel-item">
+				<h2>thing 3</h2>
+			</div>
+		
+		
+		</div>
+	</section>
+	
+</body>
+
+
+</html>
